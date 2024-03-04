dcd2330f4f2abc23654a27245b63a95b186bbccb
waqashussain2k22 <102234479+waqashussain2k22@users.noreply.github.com>
2024-03-04 19:23:46 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,377 @@
+var data = [
+        {
+            No: 1,
+            Institute: "Oakridge Manor Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 89,
+            NonResidentialStudents: 38,
+            ResidentialStudents: 51
+        },
+        {
+            No: 2,
+            Institute: "Cedarwood House School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 199,
+            NonResidentialStudents: 137,
+            ResidentialStudents: 62
+        },
+        {
+            No: 3,
+            Institute: "Pinecrest Hall School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 144,
+            NonResidentialStudents: 144,
+            ResidentialStudents: 0
+        },
+        {
+            No: 4,
+            Institute: "Willowbrook Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 224,
+            NonResidentialStudents: 125,
+            ResidentialStudents: 99
+        },
+        {
+            No: 5,
+            Institute: "Maplehurst Preparatory School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 70,
+            NonResidentialStudents: 70,
+            ResidentialStudents: 0
+        },
+        { 
+            No: 6,
+            Institute: "Lakeview Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 44,
+            NonResidentialStudents: 44,
+            ResidentialStudents: 0
+        },
+        {
+            No: 7,
+            Institute: "Prestonwood Hall School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 17,
+            NonResidentialStudents: 0,
+            ResidentialStudents: 17
+        },
+        {
+            No: 8,
+            Institute: "Brookside Manor Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 120,
+            NonResidentialStudents: 32,
+            ResidentialStudents: 88
+        },
+        {
+            No: 9,
+            Institute: "Greenwood House School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 205,
+            NonResidentialStudents: 205,
+            ResidentialStudents: 0
+        },
+        {
+            No: 10,
+            Institute: "Hillside Preparatory School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 62,
+            NonResidentialStudents: 62,
+            ResidentialStudents: 0
+        },
+        {
+            No: 11,
+            Institute: "Lakewood Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 23,
+            NonResidentialStudents: 23,
+            ResidentialStudents: 0
+        },
+        {
+            No: 12,
+            Institute: "Riverside Manor Academy",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 17,
+            NonResidentialStudents: 17,
+            ResidentialStudents: 0
+        },
+        {
+            No: 13,
+            Institute: "Fairview Hall School",
+            District: "Hyderabad Metropolitan City",
+            Division: "Hyderabad",
+            TotalStudents: 33,
+            NonResidentialStudents: 13,
+            ResidentialStudents: 20
+        },
+        {
+            No: 14,
+            Institute: "Springfield Preparatory School",
+            District: "Jamshoro",
+            Division: "Hyderabad",
+            TotalStudents: 151,
+            NonResidentialStudents: 84,
+            ResidentialStudents: 67
+        },
+        {
+            No: 15,
+            Institute: "Valleyview Academy",
+            District: "Jamshoro",
+            Division: "Hyderabad",
+            TotalStudents: 36,
+            NonResidentialStudents: 5,
+            ResidentialStudents: 31
+        },
+        {
+            No: 16,
+            Institute: "Hawthorn House School",
+            District: "Tando Allahyar",
+            Division: "Hyderabad",
+            TotalStudents: 25,
+            NonResidentialStudents: 5,
+            ResidentialStudents: 20
+        },
+        {
+            No: 17,
+            Institute: "Windsor Manor Academy",
+            District: "Tando Muhammad Khan",
+            Division: "Hyderabad",
+            TotalStudents: 123,
+            NonResidentialStudents: 72,
+            ResidentialStudents: 51
+        },
+        {
+            No: 18,
+            Institute: "Sunnyside Preparatory School",
+            District: "Meerpur Khas",
+            Division: "Meerpur Khas",
+            TotalStudents: 32,
+            NonResidentialStudents: 4,
+            ResidentialStudents: 28
+        },
+        {
+            No: 19,
+            Institute: "Hilltop Academy",
+            District: "Meerpur Khas",
+            Division: "Meerpur Khas",
+            TotalStudents: 36,
+            NonResidentialStudents: 2,
+            ResidentialStudents: 34
+        },
+        {
+            No: 20,
+            Institute: "Oakridge Manor Academy",
+            District: "Meerpur Khas",
+            Division: "Meerpur Khas",
+            TotalStudents: 42,
+            NonResidentialStudents: 6,
+            ResidentialStudents: 36
+        },
+        {
+            No: 21,
+            Institute: "Oakridge Manor Academy",
+            District: "Meerpur Khas",
+            Division: "Meerpur Khas",
+            TotalStudents: 20,
+            NonResidentialStudents: 1,
+            ResidentialStudents: 19
+        },
+        {
+            No: 22,
+            Institute: "Oakridge Manor Academy",
+            District: "Meerpur Khas",
+            Division: "Meerpur Khas",
+            TotalStudents: 113,
+            NonResidentialStudents: 12,
+            ResidentialStudents: 101
+        },
+        {
+            No: 23,
+            Institute: "Oakridge Manor Academy",
+            District: "Umar Kot",
+            Division: "Meerpur Khas",
+            TotalStudents: 49,
+            NonResidentialStudents: 16,
+            ResidentialStudents: 33
+        },
+        {
+            No: 24,
+            Institute: "Oakridge Manor Academy",
+            District: "Umar Kot",
+            Division: "Meerpur Khas",
+            TotalStudents: 145,
+            NonResidentialStudents: 78,
+            ResidentialStudents: 67
+        },
+        {
+            No: 25,
+            Institute: "Oakridge Manor Academy",
+            District: "Tharparkar",
+            Division: "Meerpur Khas",
+            TotalStudents: 80,
+            NonResidentialStudents: 47,
+            ResidentialStudents: 33
+        },
+        {
+            No: 26,
+            Institute: "Oakridge Manor Academy",
+            District: "Tharparkar",
+            Division: "Meerpur Khas",
+            TotalStudents: 32,
+            NonResidentialStudents: 12,
+            ResidentialStudents: 20
+        },
+        {
+            No: 27,
+            Institute: "Oakridge Manor Academy",
+            District: "Badin",
+            Division: "Bhanbhore",
+            TotalStudents: 64,
+            NonResidentialStudents: 23,
+            ResidentialStudents: 41
+        },
+        {
+            No: 28,
+            Institute: "Oakridge Manor Academy",
+            District: "Badin",
+            Division: "Bhanbhore",
+            TotalStudents: 129,
+            NonResidentialStudents: 18,
+            ResidentialStudents: 111
+        },
+        {
+            No: 29,
+            Institute: "Oakridge Manor Academy",
+            District: "Badin",
+            Division: "Bhanbhore",
+            TotalStudents: 128,
+            NonResidentialStudents: 15,
+            ResidentialStudents: 113
+        },
+        {
+            No: 30,
+            Institute: "Oakridge Manor Academy",
+            District: "Sajawal",
+            Division: "Bhanbhore",
+            TotalStudents: 45,
+            NonResidentialStudents: 7,
+            ResidentialStudents: 38
+        },
+        {
+            No: 31,
+            Institute: "Oakridge Manor Academy",
+            District: "Thatha",
+            Division: "Bhanbhore",
+            TotalStudents: 46,
+            NonResidentialStudents: 9,
+            ResidentialStudents: 37
+        },
+        {
+            No: 32,
+            Institute: "Oakridge Manor Academy",
+            District: "Thatha",
+            Division: "Bhanbhore",
+            TotalStudents: 37,
+            NonResidentialStudents: 0,
+            ResidentialStudents: 37
+        },
+     
+    ];
+    
+
+
+
+var tableData = document.getElementById("tabledata");
+var dataSelect = document.getElementById("data");
+var Districtdata = document.getElementById("Districtdata");
+
+var uniqueDivisions = new Set();
+var uniqueDistricts = new Set();
+
+for (var i = 0; i < data.length; i++) {
+    uniqueDivisions.add(data[i].Division);
+}
+
+uniqueDivisions.forEach(division => {
+    dataSelect.innerHTML += `<option value="${division}">${division}</option>`;
+});
+
+dataSelect.addEventListener("change", function () {
+    tableData.innerHTML = "";
+
+    var selectedDivision = dataSelect.value;
+
+    Districtdata.innerHTML = `<option value="" selected disabled>Select District</option>`;
+
+
+    uniqueDistricts.clear();
+    for (var k = 0; k < data.length; k++) {
+        if (selectedDivision === "" || data[k].Division === selectedDivision) {
+            uniqueDistricts.add(data[k].District);
+        }
+    }
+
+
+    uniqueDistricts.forEach(district => {
+        Districtdata.innerHTML += `<option value="${district}">${district}</option>`;
+    });
+
+
+    updateTable();
+});
+
+
+Districtdata.addEventListener("change", updateTable);
+
+function updateTable() {
+ 
+    tableData.innerHTML = "";
+
+
+    var selectedDivision = dataSelect.value;
+    console.log(selectedDivision);
+    var selectedDistrict = Districtdata.value;
+console.log(selectedDistrict);
+
+    for (var j = 0; j < data.length; j++) {
+        if (
+            (selectedDivision === "" || data[j].Division === selectedDivision) &&
+            (selectedDistrict === "" || data[j].District === selectedDistrict)
+        ) {
+            tableData.innerHTML += `
+            <tr style="background-color: #DCE0D9;
+            color: #000000;">
+    <td>${data[j].No}</td>
+    <td>${data[j].Institute}</td>
+    <td>${data[j].Division}</td>
+    <td>${data[j].District}</td>
+    <td>${data[j].ResidentialStudents}</td>
+    <td>${data[j].NonResidentialStudents}</td>
+    <td>${data[j].TotalStudents}</td>
+   
+    
+</tr>`
+
+
+
+}
+}
+}
+
+
+
+        
+  
+
